91625b224ca0a08d002683a78304a8514928c39a
LucasCruz <lca.snf21@uea.edu.br>
2022-08-30 12:16:35 -0400

    update

--- JantarDosFilosofosComClasse.py
+++ JantarDosFilosofosComClasse.py
@@ -8,53 +8,54 @@ garfo3 = threading.Semaphore()
 garfo4 = threading.Semaphore()
 garfo5 = threading.Semaphore()
 
-lista = [0, 0, 0, 0, 0]
+garfo1.release
+lista = []
+
 class filosofo(threading.Thread):
-    def __init__(self,  GarfoDireito, GarfoEsquerdo, nome , index):
+    def __init__(self,  GarfoDireito, GarfoEsquerdo, nome):
         threading.Thread.__init__(self)
         self.GarfoDireito = GarfoDireito
         self.GarfoEsquerdo = GarfoEsquerdo
         self.nome = nome
-        self.index = index
 
     def Pensar(self):
-        random1 = random.uniform(5, 15)
-        print(f'Pensando {self.nome}')
+        random1 = random.randint(1, 3)
+        print(f'{self.nome} Pensando por {random1} segundos')
         time.sleep(random1);
 
     def Comer(self):
         print(f"{self.nome} Tenta pegar o garfo direito")
         self.GarfoDireito.acquire()
+        
         print(f"{self.nome} Tenta pegar o garfo esquerdo")
-        locked = self.GarfoEsquerdo.acquire(False)
-        if locked:
-            print(f"{self.nome} Devolve o garfo direito")
-            self.GarfoDireito.release()
-            return
+        print(self.GarfoEsquerdo.acquire(True, 5))
+        
         print(f"{self.nome} Comendo")
-        lista[self.index] = 1
-        print(lista)
-        time.sleep(random.uniform(5, 15)) 
+        time.sleep(random.randint(1, 3)) 
+        
+        print(f"{self.nome} larga garfo direito")
         self.GarfoDireito.release()
+        
+        print(f"{self.nome} larga garfo esquerdo")
         self.GarfoEsquerdo.release()
-        lista[self.index] = 0
+        
         
     def run(self):
-        while(True):
-           
-            self.Pensar()
+        for i in range(10):
             self.Comer()
+            self.Pensar()
+            
             
   
 
 
-filosofo1 = filosofo(garfo1, garfo2, "f1", 0)
+filosofo1 = filosofo(garfo1, garfo2, "f1")
 filosofo1.start()
-filosofo2 = filosofo(garfo2, garfo3, "f2", 1)
+filosofo2 = filosofo(garfo2, garfo3, "f2")
 filosofo2.start()
-filosofo3 = filosofo(garfo3, garfo4, "f3", 2)
+filosofo3 = filosofo(garfo3, garfo4, "f3")
 filosofo3.start()
-filosofo4 = filosofo(garfo4, garfo5, "f4", 3)
+filosofo4 = filosofo(garfo4, garfo5, "f4")
 filosofo4.start()
-filosofo5 = filosofo(garfo5, garfo1, "f5", 4)
+filosofo5 = filosofo(garfo5, garfo1, "f5")
 filosofo5.start()
