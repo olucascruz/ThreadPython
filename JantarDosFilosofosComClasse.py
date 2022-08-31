ce146d2a0fba4e4eaae48839fb54a92a4a2e806e
LucasCruz <lca.snf21@uea.edu.br>
2022-08-31 02:27:34 -0400

    Documentado

--- JantarDosFilosofosComClasse.py
+++ JantarDosFilosofosComClasse.py
@@ -2,53 +2,71 @@ import random
 import threading
 import time
 
+# Cada garfo é um semaforo
+
 garfo1 = threading.Semaphore()
 garfo2 = threading.Semaphore()
 garfo3 = threading.Semaphore()
 garfo4 = threading.Semaphore()
 garfo5 = threading.Semaphore()
 
-garfo1.release
-lista = []
-
-class filosofo(threading.Thread):
-    def __init__(self,  GarfoDireito, GarfoEsquerdo, nome):
+class filosofo(threading.Thread):                           #Classe filosofo
+    def __init__(self,  GarfoDireito, GarfoEsquerdo, nome): 
         threading.Thread.__init__(self)
         self.GarfoDireito = GarfoDireito
         self.GarfoEsquerdo = GarfoEsquerdo
         self.nome = nome
 
-    def Pensar(self):
-        random1 = random.randint(1, 3)
-        print(f'{self.nome} Pensando por {random1} segundos')
-        time.sleep(random1);
+    def Pensar(self):                                        #Função pensar
+        #O filosofo pensa por um tempo aleatorio
+        tempo_pensando = random.randint(1, 3)
+        print(f'{self.nome} Pensando por {tempo_pensando} segundos ')
+        time.sleep(tempo_pensando);
 
-    def Comer(self):
-        print(f"{self.nome} Tenta pegar o garfo direito")
-        self.GarfoDireito.acquire()
+    def Comer(self):                                          #Função comer
         
-        print(f"{self.nome} Tenta pegar o garfo esquerdo")
-        print(self.GarfoEsquerdo.acquire(True, 5))
+        #Sinaliza está usando o garfo a direita
+        print(f"{self.nome} pega o garfo direito ")             
+        self.GarfoDireito.acquire()                        
         
-        print(f"{self.nome} Comendo")
-        time.sleep(random.randint(1, 3)) 
         
-        print(f"{self.nome} larga garfo direito")
+        # Tenta usar o garfo a esquerda caso esteja ocupado 
+        # espera por 2 segundos caso não libere larga o 
+        # garfo direito
+        print(f"{self.nome} Tenta pegar o garfo esquerdo ")
+        tempo_esperando_liberar_garfo = 2
+        pegar_garfo_esquerdo = self.GarfoEsquerdo.acquire(True,  tempo_esperando_liberar_garfo) 
+
+        if(not pegar_garfo_esquerdo):
+            print(f"{self.nome} larga garfo direito ")
+            self.GarfoDireito.release()
+
+
+        # Se consegue pegar os dois garfos come por um tempo 
+        # aleatorio
+        tempo_comendo = random.randint(1, 3)
+        print(f"{self.nome} Comendo por {tempo_comendo} segundos ")
+        time.sleep(tempo_comendo) 
+        
+        # Ao acabar de comer larga os garfos permitindo
+        # que os filosofos ao seu lado possam comer (usar os recursos)
+        print(f"{self.nome} larga garfo direito ")
         self.GarfoDireito.release()
         
-        print(f"{self.nome} larga garfo esquerdo")
+        print(f"{self.nome} larga garfo esquerdo ")
         self.GarfoEsquerdo.release()
         
         
     def run(self):
-        for i in range(10):
+        #Alterna entre comer e pensar
+        while True:
             self.Comer()
             self.Pensar()
             
             
   
-
-
+#Instancia ss filosofos e determina quais 
+#os garfos de cada um.
 filosofo1 = filosofo(garfo1, garfo2, "f1")
 filosofo1.start()
 filosofo2 = filosofo(garfo2, garfo3, "f2")
